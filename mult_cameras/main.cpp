d49e4fc7c4e488980343590c0ed5f36d4d8c7426
Peyton Randolph <peyton@cmu.edu>
2012-10-19 12:24:36 -0400

    Update .gitignore

--- mult_cameras/main.cpp
+++ mult_cameras/main.cpp
@@ -10,7 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Peyton Randolph (), 
+ *         Author:  Peyton Randolph (),
  *   Organization:  Carnegie Mellon University
  *
  * =====================================================================================
@@ -19,11 +19,28 @@
 #include <pcl/io/openni_grabber.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/console/parse.h>
+
+void
+printUsage (const char* prog_name)
+{
+  std::cout << "\n\nUsage: "<<prog_name<<" [options]\n\n"
+    << "Options:\n"
+    << "--------------------------------------\n"
+    << "-h          this help\n"
+    << "-n          number of cameras\n"
+    << "\n\n";
+}
 
 class SimpleOpenNIViewer
 {
+  protected:
+    int num_cameras_;
+
   public:
-    SimpleOpenNIViewer () : viewer ("PCL OpenNI Viewer") {}
+    SimpleOpenNIViewer (int num_cameras = 1) : viewer ("PCL OpenNI Viewer") {
+      num_cameras_ = num_cameras;
+    }
 
     void cloud_cb_2_ (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr &cloud)
     {
@@ -50,9 +67,10 @@ class SimpleOpenNIViewer
       interface->start();
       //* *********************************
 
-      //* *********************************  
+      //* *********************************
       //start second kinect
-      pcl::Grabber* interface2 = new pcl::OpenNIGrabber("#2");  
+      pcl::Grabber* interface2 = new pcl::OpenNIGrabber("#2");
+
       boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f2 =
         boost::bind (&SimpleOpenNIViewer::cloud_cb_2_, this, _1);
 
@@ -74,9 +92,28 @@ class SimpleOpenNIViewer
     pcl::visualization::CloudViewer viewer;
 };
 
-int main ()
+int
+main (int argc, char** argv)
 {
-  SimpleOpenNIViewer v;
+  // Default values
+  int num_cameras(1);
+
+  /*
+   * Parse command line arguments
+   */
+  if (pcl::console::find_argument (argc, argv, "-h") >= 0)
+  {
+    printUsage (argv[0]);
+    return 0;
+  }
+
+  // Number of cameras
+  if (pcl::console::find_argument(argc, argv, "-n") >= 0)
+  {
+    pcl::console::parse_argument(argc, argv, "-n", num_cameras);
+  }
+
+  SimpleOpenNIViewer v (num_cameras);
   v.run ();
   return 0;
 }
